2a6fd4a13f83c785c90039e3fbebb2ebdcdb18db
Darek D <darekduda21@gmail.com>
2022-12-17 10:31:53 +0100

    Add BUS implementation with tests

---
+++ src/riscv_py_emu/bus/controller.py
@@ -0,0 +1,17 @@
+from riscv_py_emu.dram.controller import DramController
+from riscv_py_emu.dram.memory import OperationSize
+
+
+class BusController:
+    """Class that handles accessing memory via BUS."""
+
+    def __init__(self, dram_controller: DramController) -> None:
+        self._dram_controller = dram_controller
+
+    def load(self, address: int, *, size: OperationSize) -> int:
+        """Load data of given size from memory."""
+        return self._dram_controller.load(address, size=size)
+
+    def store(self, address: int, *, size: OperationSize, value: int) -> None:
+        """Store value of given size in memory."""
+        return self._dram_controller.store(address, size=size, value=value)
--- tests/test_bus_controller.py
+++ tests/test_bus_controller.py
@@ -1,5 +1,6 @@
 import pytest
 
+from riscv_py_emu.bus.controller import BusController
 from riscv_py_emu.dram.controller import DramController
 from riscv_py_emu.dram.exception import InvalidOperatorSize, MemoryOutOfRange
 from riscv_py_emu.dram.memory import Dram, OperationSize
@@ -15,46 +16,53 @@ def dram_controller(dram_memory: Dram) -> DramController:
     yield DramController(dram_memory=dram_memory)
 
 
-def test_creating_dram_controller(dram_controller: DramController) -> None:
-    assert dram_controller, "DRAM controller cannot be initialized."
+@pytest.fixture()
+def bus_controller(dram_controller: DramController) -> BusController:
+    yield BusController(dram_controller=dram_controller)
 
 
-def test_loading_16_bytes(dram_memory: Dram, dram_controller: DramController) -> None:
+def test_creating_bus_controller(bus_controller: BusController) -> None:
+    assert bus_controller, "BUS controller cannot be initialized."
+
+
+def test_loading_16_bytes_via_bus(
+    dram_memory: Dram, bus_controller: BusController
+) -> None:
     addr = dram_memory.base + 4 * (OperationSize.UINT_64.value)
-    val = dram_controller.load(addr, size=OperationSize.UINT_16)
+    val = bus_controller.load(addr, size=OperationSize.UINT_16)
     assert val == 0, "There is no data in fake "
 
 
 def test_loading_18_bytes_raises_exception(
-    dram_memory: Dram, dram_controller: DramController
+    dram_memory: Dram, bus_controller: BusController
 ) -> None:
     addr = dram_memory.base + 4 * (OperationSize.UINT_64.value)
     with pytest.raises(InvalidOperatorSize):
-        dram_controller.load(addr, size=18)
+        bus_controller.load(addr, size=18)
 
 
-def test_loading_outside_of_memory(dram_controller: DramController) -> None:
+def test_loading_outside_of_memory(bus_controller: BusController) -> None:
     addr = 4 * (OperationSize.UINT_64.value)
     with pytest.raises(MemoryOutOfRange):
-        dram_controller.load(addr, size=OperationSize.UINT_8)
+        bus_controller.load(addr, size=OperationSize.UINT_8)
 
 
 def test_loading_outside_of_memory_upper_range(
-    dram_memory: Dram, dram_controller: DramController
+    dram_memory: Dram, bus_controller: BusController
 ) -> None:
     addr = dram_memory.base + dram_memory.size
     with pytest.raises(MemoryOutOfRange):
-        dram_controller.load(addr, size=OperationSize.UINT_8)
+        bus_controller.load(addr, size=OperationSize.UINT_8)
 
 
 def test_store_and_load_16_bytes(
-    dram_memory: Dram, dram_controller: DramController
+    dram_memory: Dram, bus_controller: BusController
 ) -> None:
     addr = dram_memory.base + 4 * (OperationSize.UINT_64.value)
     value = 1642
     value_size = OperationSize.UINT_16
-    dram_controller.store(addr, size=value_size, value=value)
-    value_restored = dram_controller.load(addr, size=value_size)
+    bus_controller.store(addr, size=value_size, value=value)
+    value_restored = bus_controller.load(addr, size=value_size)
     assert value_restored == value, (
         f"Different value was stored than loaded. "
         f"Stored {value} != Loaded {value_restored}"
